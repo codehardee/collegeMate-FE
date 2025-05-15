46d0f395abdd3e031ddca7e780cef10d2ac612de
Heetraval <heetraval26@gmail.com>
2025-05-15 19:23:19 -0400

    navbar update.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./navbar.css";
+import "./CategoryGrid.css";
 import { clearTokens } from "../authHelpers";
 
 const Navbar = () => {
